d3db60b114c280d643fd4e5d55ccc8d0b8970f3a
abdulalali <113354295+abdulalali@users.noreply.github.com>
2022-10-05 08:32:14 -0400

    java_assignment2

--- Assignment2.java
+++ Assignment2.java
@@ -1,5 +1,76 @@
+import java.util.Scanner;
+
+
 public class Assignment2 {
-    public static void main(String[] args) {
-        System.out.println();
+    public static void main(String [] args){
+        int n;
+        int a;
+        int i;
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter a number");
+        n = in.nextInt();
+        
+        if(n % 2 == 0){
+            for(i=0; i <= n/2; i++)
+                System.out.print("  ");
+            System.out.println("*");
+            
+            a = 2;
+            
+            while(a <= n){
+                for(i=0; i<=(n/2 - a/2); i++)
+                    System.out.print("  ");
+                for(i=0; i<a; i++)
+                    System.out.print(" *");
+                System.out.println();
+                a += 2;
+            }
+            
+            a -= 4;
+            while(a > 1){
+                for(i=0; i<=(n/2 - a/2); i++)
+                    System.out.print("  ");
+                for(i=0; i<a; i++)
+                    System.out.print(" *");
+                System.out.println();
+                a -= 2;
+            }
+            
+            for(i=0; i <= n/2; i++)
+                System.out.print("  ");
+            System.out.println("*");
+        }
+        else{
+            for(i=0; i <= n/2; i++)
+                System.out.print("  ");
+            System.out.println("*");
+            
+            a = 3;
+            
+            while(a <= n){
+                for(i=0; i<=(n/2 - a/2); i++)
+                    System.out.print("  ");
+                for(i=0; i<a; i++)
+                    System.out.print("* ");
+                System.out.println();
+                a += 2;
+            }
+            
+            a -= 4;
+            while(a > 1){
+                for(i=0; i<=(n/2 - a/2); i++)
+                    System.out.print("  ");
+                for(i=0; i<a; i++)
+                    System.out.print("* ");
+                System.out.println();
+                a -= 2;
+            }
+            
+            for(i=0; i <= n/2; i++)
+                System.out.print("  ");
+            System.out.println("*");
+        }
+        
+        
     }
 }
